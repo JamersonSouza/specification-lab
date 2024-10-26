bbf1cf0fd8b6fc90d54647bcc1c8a29187bb6644
jamersonsouza <jamersonsouza@outlook.com>
2024-10-25 21:51:35 -0300

    init project specification

---
+++ src/main/java/tech/jamersondev/specification_lab/SpecificationLabApplication.java
@@ -0,0 +1,13 @@
+package tech.jamersondev.specification_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecificationLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecificationLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/jamersondev/specification_lab/SpecificationLabApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.jamersondev.specification_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecificationLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
